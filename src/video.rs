491e8717bb9e8d38e34f28e3cf4611f1724f272a
enpotid <xian101221@gmail.com>
2025-08-25 22:26:11 +0900

    add fields to Video

--- src/video.rs
+++ src/video.rs
@@ -1,7 +1,25 @@
-pub struct Video {}
+use std::path::PathBuf;
+
+pub struct Video {
+    pub path: PathBuf,
+    pub resolution: Resolution,
+    pub fps: usize,
+}
+
+pub struct Resolution {
+    pub width: usize,
+    pub height: usize,
+}
 
 impl Video {
-    pub fn new() -> Self {
-        Video {}
+    pub fn new(path: impl Into<PathBuf>, resolution: (usize, usize), fps: usize) -> Self {
+        Video {
+            path: path.into(),
+            resolution: Resolution {
+                width: resolution.0,
+                height: resolution.1,
+            },
+            fps,
+        }
     }
 }
